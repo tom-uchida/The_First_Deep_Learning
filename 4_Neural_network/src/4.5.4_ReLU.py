3a6b6ef7fcc7b2dc00b7f50b7899506ca69cc38c
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2019-05-11 15:30:58 +0900

    update

--- 4_Neural_network/src/4.5.4_ReLU.py
+++ 4_Neural_network/src/4.5.4_ReLU.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sigmoid_function(x):
-    return 1/(1+np.exp(-x))
+def ReLU_function(x):
+    return np.where(x<=0, 0, x)
 
 x = np.linspace(-5, 5)
-y = sigmoid_function(x)
+y = ReLU_function(x)
 
 plt.plot(x, y)
 plt.show()
